2952f0f593658696ba2089d94ab8dbb19f7a72b8
G00419525@atu.ie <G00419525@atu.ie>
2024-10-09 15:12:23 +0100

    Added code to create a person and if an error occurs then an adviser instructs Spring Boot to handle specific situations.

--- src/main/java/ie/atu/lab4_cicd/Person.java
+++ src/main/java/ie/atu/lab4_cicd/Person.java
@@ -1,4 +1,28 @@
 package ie.atu.lab4_cicd;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+    @NotBlank(message = "You must enter a name.")
+    private String name;
+    @NotBlank(message = "You must enter a title")
+    private String title;
+    @Email(message = "Not a valid email address")
+    private String email;
+    @NotBlank(message = "Not a position")
+    private String position;
+    @NotBlank(message = "Not a department")
+    private String department;
+    @Min(value = 0, message = "Age must be greater or equal to 0")
+    private int age;
+    @Min(value = 1, message = "Must be greater or equal to 1")
+    private int employeeId;
 }
